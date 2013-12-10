bd0a15ab46c5e9f4004881d5923783fcfa1ddaa9
ryan chew <ryan.layosh@gmail.com>
2013-12-10 23:59:44 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moneybaby::Application.config.secret_key_base = 'c11b066d8adb44ac6f16384680ef210ac2224526e7864ec58ab39ee497c7f13a09ea6ddb9b3a9783cf94efc6d012acb64ee1bbb81a3dce65790d9313f3e37353'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Moneybaby::Application.config.session_store :cookie_store, key: '_moneybaby_session'
